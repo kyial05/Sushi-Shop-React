3bb0b7998740c460435e8bc70263c5db26a51a9d
kyial05 <asuskyial@gmail.com>
2023-05-08 15:48:38 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Sushi } from "./Components/Main";
+import { Dat } from "./Components/Main/data";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Dat.map((el, id) => {
+        return <Sushi el={el} id={id} />;
+      })}
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/Components/Main/data.js
@@ -0,0 +1,24 @@
+import first from "../Img/first.jpg";
+import second from "../Img/second.jpg";
+import third from "../Img/third.jpg";
+
+export const Dat = [
+  {
+    id: 1,
+    img: first,
+    price: 20,
+    des: "Fedele",
+  },
+  {
+    id: 2,
+    img: second,
+    price: 30,
+    des: "Filadelfi",
+  },
+  {
+    id: 3,
+    img: third,
+    price: 40,
+    des: "Maki",
+  },
+];
